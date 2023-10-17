be4089adb3e7dada7477b265b9145ff7bad96bbe
ctbus <ctbushman@gmail.com>
2023-10-17 16:16:36 -0400

    Add first dataset

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Contig Cluster 1
+#'
+#' Data associated with DEMIC paper (on SourceForge)
+#'
+#' @format ## `ContigCluster1`
+#' A data frame with 120,897 rows and 5 columns:
+#' \describe{
+#'   \item{log_cov}{Log Coverage for Sliding Windows over Contigs}
+#'   \item{GC_content}{GC Content for Sliding Windows over Contigs}
+#'   \item{sample}{Sample Name}
+#'   \item{contig}{Contig Name}
+#'   \item{length}{Length of Contig}
+#' }
+#' @source <https://sourceforge.net/projects/demic/files/>
+"ContigCluster1"
