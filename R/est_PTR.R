3f21ca1c436a6dd2a51fddb1239e6e5a4b16ce5b
Ulthran <ctbushman@gmail.com>
2024-01-18 09:34:56 -0500

    Update docs slightly

--- R/est_PTR.R
+++ R/est_PTR.R
@@ -6,13 +6,11 @@ demic_env$MAX_ITER <- 3
 #' Estimate PTRs from both contigs and samples
 #'
 #' @param X dataframe with coverage matrix
-#' (column names: "logCov", "GC", "sample", "contig", "length")
-#' @param max_candidate_iter max allowed iterations for estimation of PTR
-#' (default: 10)
+#' (column names: "log_cov", "GC_content", "sample", "contig", "length")
 #' @return named list with results from both methods
 #' contigs_ptr dataframe with the estimated PTRs
 #' \itemize{
-#'   \item estPTR: estimated PTR values
+#'   \item est_ptr: estimated PTR values
 #'   \item coefficient: coefficient of linear regression
 #'   \item pValue: p-value of linear regression
 #'   \item cor: correlation coefficient
@@ -20,7 +18,7 @@ demic_env$MAX_ITER <- 3
 #' }
 #' samples_ptr dataframe with the estimated PTRs
 #' \itemize{
-#'   \item estPTR: estimated PTR values
+#'   \item est_ptr: estimated PTR values
 #'   \item coefficient: coefficient of linear regression
 #'   \item pValue: p-value of linear regression
 #'   \item cor: correlation coefficient
@@ -32,12 +30,22 @@ demic_env$MAX_ITER <- 3
 #' est_ptrs_001
 #'
 #' @export
-est_ptr <- function(X, max_candidate_iter = 10) {
+est_ptr <- function(X) {
   verify_input(X)
 
-  contig_est_ptrs <- est_ptr_from_contigs(X)
-  sample_est_ptrs <-
-    est_ptr_from_samples(X, max_candidate_iter = max_candidate_iter)
+  tryCatch(
+    contig_est_ptrs <- est_ptr_from_contigs(X),
+    error = function(e) {
+      message("Error in est_ptr_from_contigs: ", e)
+    }
+  )
+
+  tryCatch(
+    sample_est_ptrs <- est_ptr_from_samples(X),
+    error = function(e) {
+      message("Error in est_ptr_from_samples: ", e)
+    }
+  )
 
   est_ptrs <- combine_ests(contig_est_ptrs, sample_est_ptrs)
 
